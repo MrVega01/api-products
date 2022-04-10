4b5fe51407114e39bbdf431e7ca6c029062c1904
MrVega01 <ignaciovega2003@outlook.com>
2022-04-10 15:23:09 -0400

    Dinamic config for Heroku deploy and local start

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ app.use((req, res) => {
     })
 })
 //Server listener
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, ()=>{
     console.log(`Server running in: localhost:${PORT}`);
 })
